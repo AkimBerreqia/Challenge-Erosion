9cee24766514f98eb102ada495724939f403aff5
AkimBerreqia <berreqia.akim@gmail.com>
2023-02-13 17:16:42 +0000

    list comprehension done wonderfully

--- src/rendu.py
+++ src/rendu.py
@@ -28,16 +28,7 @@ def img2ascii(img_data, black = '#', white = '.') -> str:
     ..........
     '''
 
-    final_img = ''
-    for line in img_data:
-        str_ln = ''
-        for pixel in line:
-            if pixel == 0:
-                str_ln += white
-            elif pixel == 1:
-                str_ln += black
-        final_img += f'{str_ln}\n'
-    return ''.join([white if pixel == 0 else black for line in img_data for pixel in line])
+    return '\n'.join([''.join([white if pixel == 0 else black for pixel in line]) for line in img_data])
 
 img = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -58,6 +49,17 @@ print(ascii)
 
 
 
-if __name__ =='__main__':
+'''if __name__ =='__main__':
     import doctest
     doctest.testmod()
+    '''
+tests = [
+    '..........\n..######..\n.##....##.\n.#......#.\n.#......#.\n.#......#.\n.#......#.\n.##....##.\n..######..\n..........',
+    '          \n  OOOOOO  \n OO    OO \n O      O \n O      O \n O      O \n O      O \n OO    OO \n  OOOOOO  \n          ',
+
+    ]
+
+if tests[0] == img2ascii(img):
+    print('bien !!!')
+if tests[1] == img2ascii(img, 'O', ' '):
+    print('tout de bien')
